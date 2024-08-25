6ca2ab63abc30f9d9439b1d0cf8a2d54c6b375c8
Falkgaard <victor.itzel@gmail.com>
2024-08-25 18:29:41 +0200

    Delete homeManagerModules/features/chromium.nix

--- homeManagerModules/features/chromium.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-
-  programs.chromium.enable = true;
-  programs.chromium.package = pkgs.ungoogled-chromium;
-
-  myHomeManager.impermanence.cache.directories = [
-    ".config/chromium"
-  ];
-}
