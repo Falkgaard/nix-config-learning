e882155f59c380c9d9a61aaeb3ee2315d449775a
Falkgaard <victor.itzel@gmail.com>
2024-09-04 05:28:28 +0200

    Update default.nix

--- v2/home/user.profiles/falk/profiles/workflow.profiles/graphics.profile/default.nix
+++ v2/home/user.profiles/falk/profiles/workflow.profiles/graphics.profile/default.nix
@@ -2,7 +2,7 @@
 {
    # TODO: Ideally I'd want the option to disable specific submodules...
    import = [
-      ./blender.submodule.nix             (move into `3d-graphics.profile`?)
+      ./blender.submodule.nix           # (move into `3d-graphics.profile`?)
       ./krita.submodule.nix
       # TODO: libresprite (FOSS aseprite) (move into `pixel-art.profile`?)
       #       inkscape?                   (move into `vector-graphics.profile`?)
