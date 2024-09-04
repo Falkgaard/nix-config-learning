1ba47ea26103ceafe00d9a32c0b99906c0f22930
Falkgaard <victor.itzel@gmail.com>
2024-09-04 04:51:01 +0200

    Update default.nix

--- v2/home/user.profiles/falk/profiles/core.profile/default.nix
+++ v2/home/user.profiles/falk/profiles/core.profile/default.nix
@@ -1,3 +1,4 @@
+# TODO: Move firefox into core? Maybe make firefox a submodule of `web-browser.profile` that imports it (that way if I ever decide to change my default browser I just have to change it there9.
 {
    imports = [
        ./locale.nix
