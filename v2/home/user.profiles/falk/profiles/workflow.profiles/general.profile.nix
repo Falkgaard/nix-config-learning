4ed22b69de101472bb92be4657e9edecae05545a
Falkgaard <victor.itzel@gmail.com>
2024-09-04 05:15:30 +0200

    Update general.profile.nix
    Remove zathura (moving to office profile)

--- v2/home/user.profiles/falk/profiles/workflow.profiles/general.profile.nix
+++ v2/home/user.profiles/falk/profiles/workflow.profiles/general.profile.nix
@@ -1,5 +1,4 @@
 # git
-# zathura?
 # web-browser submodule (default to firefox)                  (turn into web-browser.profile?)   (NOTE: not in core since it requires a GUI)
 # terminal submodule (default to kitty + fish + starship?)    (turn into terminal.profile?)      (NOTE: not in core since kitty requires a GUI)
 #     ^ also include yazi, bat, tree, tldr, ripgrep, etc 
