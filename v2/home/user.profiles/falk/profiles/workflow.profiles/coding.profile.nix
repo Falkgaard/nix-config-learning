ef04dc90f5796a40467614ca65216f6303482ad5
Falkgaard <victor.itzel@gmail.com>
2024-09-04 04:45:43 +0200

    Update coding.profile.nix
    Skeleton roadmap

---
+++ v2/home/user.profiles/falk/profiles/workflow.profiles/coding.profile.nix
@@ -0,0 +1,50 @@
+# TODO: Refactor into a module directory
+#       Create submodules for different languages:
+#           1. C/C++/C3C
+#           2. Rust
+#           4. Lua(JIT)
+#           5. OpenGL/Vulkan/GLSL? (handle as options to other languages?)
+#              ...
+#           etc
+
+{
+   # TODO:
+   #    neovim + plugins
+   #    code-oss? (vscode)
+}
+
+# C/C++/C3:
+#
+#    clang, gcc, c3c
+#    gdb, lldb, gdb-frontend ...
+#    ASan, MSan, TSan ...
+#    cpp-check?
+#    valgrind
+#    make, cmake...
+#    clangd, ccls
+#    clang-tidy
+#    clang-format
+#    emscripten? wasm?
+#    nvim plugins... (overlay?)
+#    dev shell...
+
+# Rust:
+#
+#    rustc? rustup?
+#    rustfmt
+#    rust-bindgen?
+#    rust-analyzer
+#    rust-code-analysis?
+#    wasm-tools?
+#    nvim plugins... (overlay?)
+#    dev shell...
+
+# Lua:
+#
+#    lua, luau, luajit,
+#    luabind, luabind_luajit, luabridge?
+#    luaformatter
+#    nvim plugins... (overlay?)
+#    dev shell...
+
+# TODO: Decide on how to handle overlap (e.g. Lua|LuaJiT + C++ Sol2 ...
