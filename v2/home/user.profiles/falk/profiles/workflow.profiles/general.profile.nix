e7ce2363a1238d8b4bb87c87dc0d47af19fe0e1d
Falkgaard <victor.itzel@gmail.com>
2024-09-04 04:58:54 +0200

    Update general.profile.nix
    Skeleton roadmap

--- v2/home/user.profiles/falk/profiles/workflow.profiles/general.profile.nix
+++ v2/home/user.profiles/falk/profiles/workflow.profiles/general.profile.nix
@@ -1,1 +1,5 @@
-# Firefox... anything else?
+# git
+# zathura?
+# web-browser submodule (default to firefox) alt. move into a `web.profile`   (NOTE: not in core since it requiresa GUI)
+# terminal submodule (default to kitty + fish + starship?)                    (NOTE: not in core since it requiresa GUI)
+#     ^ also include yazi, bat, tree, tldr, ripgrep, etc 
