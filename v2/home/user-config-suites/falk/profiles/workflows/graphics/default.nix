a53ecf196162cdd968bc78094c73f11773398924
Falkgaard <falkgaard@gmail.com>
2024-09-04 13:10:12 +0200

    Restructuring

--- v2/home/user-config-suites/falk/profiles/workflows/graphics/default.nix
+++ v2/home/user-config-suites/falk/profiles/workflows/graphics/default.nix
@@ -1,13 +1,13 @@
 {...}:
 {
    # TODO: Ideally I'd want the option to disable specific submodules...
-   import = [
-      ./blender.nix # (move into `3d-graphics.profile`?)
-      ./krita.nix   # (move into `2d-graphics.profile`?)
-      # TODO: libresprite (FOSS aseprite) (move into `2d-graphics.profile`?)
-      #       image-magick?               (move into `2d-graphics.profile`?)
-      #       inkscape?                   (move into `2d-graphics.profile`?)
-      #       davinci-resolve?            (move into `video-editing.profile`)
-   ];
+   #import = [
+   #   ./blender.nix # (move into `3d-graphics.profile`?)
+   #   ./krita.nix   # (move into `2d-graphics.profile`?)
+   #   # TODO: libresprite (FOSS aseprite) (move into `2d-graphics.profile`?)
+   #   #       image-magick?               (move into `2d-graphics.profile`?)
+   #   #       inkscape?                   (move into `2d-graphics.profile`?)
+   #   #       davinci-resolve?            (move into `video-editing.profile`)
+   #];
 }
 
