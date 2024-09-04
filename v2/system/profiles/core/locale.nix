21453a0df1a738dd783ed7df90dc7e083eb62ffc
Falkgaard <falkgaard@gmail.com>
2024-09-04 12:56:16 +0200

    Restructuring

--- v2/system/profiles/core/locale.nix
+++ v2/system/profiles/core/locale.nix
@@ -2,19 +2,19 @@
 {
    i18n = {
       defaultLocale = lib.mkDefault "en_GB.UTF-8";
-
+      
       extraLocaleSettings = {
          LC_TIME = lib.mkDefault "en_DK.UTF-8";
       };
-
+      
       supportedLocales = lib.mkDefault [
          "en_GB.UTF-8/UTF-8"
          "en_US.UTF-8/UTF-8"
          # TODO: Swedish & Japanese.
       ];
-
-      location.provider = "geoclue2"; # TODO: Look into alternatives.
-
-      time.timeZone = lib.mkDefault "Europe/Malta";
    };
+   
+   location.provider = "geoclue2"; # TODO: Look into alternatives.
+   time.timeZone     = lib.mkDefault "Europe/Malta";
 }
+
