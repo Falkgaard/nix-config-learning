9a7f0e03a1660b6a24265ca139d7d84d875055b7
Falkgaard <victor.itzel@gmail.com>
2024-09-05 03:03:11 +0200

    Update gaming.nix
    Fleshed out the gaming profile in accordance to the Nix wiki

--- v2/home/user-config-suites/falk/profiles/workflows/gaming.nix
+++ v2/home/user-config-suites/falk/profiles/workflows/gaming.nix
@@ -1,8 +1,32 @@
-# Steam
-# Heroic
-# Lutris
-# Wine/Proton?
-# Prismlauncher (Minecraft Launcher)
-# Anything else?
-{}
-
+# TODO: Refactor out of `workflows`?
+{ pkgs, config, ... }:
+{
+   imports = [];
+   
+   options = {};
+   
+   config = {
+      programs.gamemode.enable = true; # For performance mode.
+      
+      programs.steam = {
+         enable                       = true; # Install Steam.
+         remotePlay.openFirewall      = true; # Open ports in the firewall for Steam Remote Play.
+         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server.
+      };
+      
+      environment.systemPackages = with pkgs; [
+         heroic            # Install Heroic launcher.
+         lutris            # Install Lutris launcher.
+         prismlauncher     # Install Prism Launcher (Minecraft launcher).
+         #mumble           # Install voice-chat.
+         #teamspeak_client # Install voice-chat.
+         protonup-qt       # GUI for installing custom Proton versions like GE_Proton.
+         (retroarch.override {
+            cores = with libretro; [
+               # Add more emulators here (if needed):
+               dosbox
+            ];
+         })
+      ];
+   };
+} # end-of: <module>
